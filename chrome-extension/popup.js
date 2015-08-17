64e49b2b95df8e9f3ec81eb8c950ccca1dddff0f
Nachiketa Gargi <nacgarg@nuevaschool.org>
2015-08-17 15:26:51 -0700

    Added dynamically changing fields to add metadata

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,6 +1,6 @@
 var videoId
-document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById('test').innerText = 'sdfsdfsd'
+document.addEventListener("DOMContentLoaded", function() {
+    initGenres();
     chrome.runtime.sendMessage({
         target: 'content-script'
     }, function(response) {
@@ -17,3 +17,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
         xmlhttp.send(params);
     });
 });
+
+var initGenres = function() {
+    var dropdown = $("#genres");
+    $.getJSON('genres.json', function(data) {
+        console.log(data);
+        for (var key in data) {
+            dropdown.append($('<option>' + data[key].name + '</option>')
+                .attr({
+                    value: key
+                })
+                .addClass("text")
+            );
+        };
+        dropdown.change(function(e) {
+            setFields(data[$('#genres').val()].fields);
+        });
+    })
+}
+
+var setFields = function(arr) {
+    $('#fields').html('');
+    for (var i = 0; i < arr.length; i++) {
+        $('#fields').append($('<input type="text" class="field" placeholder="'+arr[i][0].toUpperCase() + arr[i].slice(1) + '" data-tag="'+arr[i]+'"/>'));
+    };
+}
